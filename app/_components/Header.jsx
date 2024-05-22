2774f924eb035177c53fff351cf491d578c5b2e9
Francis Iwu <apkfem@gmail.com>
2024-05-22 23:40:24 +0200

    implement login authentication using Clerk services

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import React from "react";
 
 function Header() {
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-6 border-b shadow-lg">
       <div className="flex justify-between items-center">
         <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
-        <Button>Get Started</Button>
+
+        {isSignedIn ? (
+          <div className="flex items-center gap-5">
+            <Button variant="outline">Get Started</Button>
+            <UserButton />
+          </div>
+        ) : (
+          <Button>Get Started</Button>
+        )}
       </div>
     </div>
   );
